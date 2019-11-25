b9fe56e25e714292f951d53be7897f0038925479
Jonathan Prieto <jonathan.prieto@me.com>
2019-11-25 06:54:33 -0500

    uPLOAD sOME fiLES

---
+++ cmake-build-debug/CMakeFiles/i-can-guess-the-data-structure-11995-jonathanprieto99.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/i-can-guess-the-data-structure-11995-jonathanprieto99.dir/main.cpp.o"
+  "i-can-guess-the-data-structure-11995-jonathanprieto99"
+  "i-can-guess-the-data-structure-11995-jonathanprieto99.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/i-can-guess-the-data-structure-11995-jonathanprieto99.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
